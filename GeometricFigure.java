5c10b18713f932c656050083e7e2425f231f7a17
Anastasia Ravossel <velnast@gmail.com>
2023-12-30 00:57:56 +0300

    OOP first lesson

---
+++ GeometricFigure.java
@@ -0,0 +1,50 @@
+public abstract class GeometricFigure {
+    public abstract double calculateArea();
+    public abstract double calculatePerimeter();
+}
+
+class Circle extends GeometricFigure {
+    private double radius;
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double calculateArea() {
+        return Math.PI * Math.pow(radius, 2);
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * Math.PI * radius;
+    }
+}
+
+class Rectangle extends GeometricFigure {
+    private double length;
+    private double width;
+
+    public Rectangle(double length, double width) {
+        this.length = length;
+        this.width = width;
+    }
+
+    @Override
+    public double calculateArea() {
+        return length * width;
+    }
+
+    @Override
+    public double calculatePerimeter() {
+        return 2 * (length + width);
+    }
+}
+
+class Square extends Rectangle {
+    public Square(double side) {
+        super(side, side);
+    }
+}
+
+
